e98d008abc63836eb5d6f7022e030e12ddd59383
Zach Showalter <zach@casperlabs.io>
2024-05-23 21:41:05 -0400

    Remove unused import leftover from merge
    Signed-off-by: Zach Showalter <zach@casperlabs.io>

--- lib/cli/transaction.rs
+++ lib/cli/transaction.rs
@@ -5,7 +5,8 @@ use crate::{
     rpcs::results::PutTransactionResult,
     speculative_exec_txn, SuccessResponse,
 };
-use casper_types::{Digest, InitiatorAddr, Transaction, TransactionSessionKind, TransactionV1, TransactionV1Builder, U512};
+use casper_types::{
+    Digest, InitiatorAddr, Transaction, TransactionSessionKind, TransactionV1, TransactionV1Builder};
 
 pub fn create_transaction(
     builder_params: TransactionBuilderParams,
